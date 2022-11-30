09fbffe124bf80c20b9ee6fc93b4585eafd8808b
ilario-moreni <ilario2001.mor@gmail.com>
2022-11-30 15:37:11 +0100

    initiliaze and refactoring for bonus (mail)

---
+++ mail/bonus/js/script.js
@@ -0,0 +1,5 @@
+/* bonus */
+//1 - salvare il valore di input in una variabile
+//2 - controllare il valore salvato corrisponda ad un elemento dell'array
+//3 - SE è nella lista cambiare l'h1 in Benvenuto
+//3.1 - ALTRIMENTI cambiare l'h1 in 'Inserisci una mail valida'
