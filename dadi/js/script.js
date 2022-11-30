641f6a8f1ac12069c2447d9e407459414ac6b039
ilario-moreni <ilario2001.mor@gmail.com>
2022-11-30 15:27:22 +0100

    problem solved (dadi)

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,20 @@
-alert('sono linkato')
+/* 1 */
+
+let playerScore, computerScore;
+
+playerScore = Math.floor(Math.random()*6) + 1;
+
+computerScore = Math.floor(Math.random()*6) + 1;
+
+console.log(computerScore+': punteggio del computer');
+console.log(playerScore+': punteggio del giocatore');
+
+/* 2 */
+
+if (playerScore == computerScore){
+    console.log('Pareggio');
+} else if (playerScore > computerScore){
+    console.log('Hai vinto');
+} else{
+    console.log('Hai perso');
+}
