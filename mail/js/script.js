ff7543c21d0aebe4a8e6e069e6c3285a2058654d
ilario-moreni <ilario2001.mor@gmail.com>
2022-11-30 15:10:42 +0100

    problem solved (mail)

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,23 @@
-alert('js è linkato')
+/* 1 */
+
+let userMail = prompt('Inserisci la tua mail:');
+
+/* 2 */
+
+let correctMailArray = ['cicciopasticcio@mail.com', 'boolean@mail.com', 'correctmail@mail.com', 'mailgiusta@mail.com'];
+
+let mailChecked;
+
+for (let i=0; i < correctMailArray.length; i++){
+    if (correctMailArray[i] == userMail){
+        mailChecked = userMail;
+    }
+}
+
+/* 3 */
+
+if (mailChecked){
+    console.log('Benvenuto')
+} else{
+    console.log('Inserisci una mail valida')
+}
