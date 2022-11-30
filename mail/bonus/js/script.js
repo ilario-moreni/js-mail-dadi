29d5dcee8c5575b13d3f6a9a43daa8863af6305c
ilario-moreni <ilario2001.mor@gmail.com>
2022-11-30 16:38:22 +0100

    bonus fixed (mail)

--- mail/bonus/js/script.js
+++ mail/bonus/js/script.js
@@ -1,28 +1,18 @@
 /* bonus */
-//1 - salvare il valore di input in una variabile
-//2 - controllare il valore salvato corrisponda ad un elemento dell'array
-//3 - al click del pulsante cambiare l'h1 secondo le seguenti condizioni
-//3.1 - SE è nella lista cambiare l'h1 in Benvenuto
-//3.2 - ALTRIMENTI cambiare l'h1 in 'Inserisci una mail valida'
-
-/* 1 */
-
-let userMail = document.getElementById('input_mail').value;
-
-console.log(userMail);
-
-/* 2 */
-
 let correctMailArray = ['cicciopasticcio@mail.com', 'boolean@mail.com', 'correctmail@mail.com', 'mailgiusta@mail.com'];
 
-let mailChecked;
-
-
-
-/* 3 */
-
+let userMail;
 
 document.getElementById('my_button').addEventListener('click', function(){
+
+    userMail = document.getElementById('input_mail').value;
+
+    console.log(userMail);
+
+
+
+    let mailChecked;
+
     for (let i=0; i < correctMailArray.length; i++){
         if (correctMailArray[i] == userMail){
             mailChecked = userMail;
